7bc74372a52f9586031b35617f10f987387a8109
Paulo Coutinho <pcmnac@gmail.com>
2020-11-08 15:53:54 +0000

    :white_check_mark: aws commands

--- aws-util-commands/sing-up-user.sh
+++ aws-util-commands/sing-up-user.sh
@@ -1,5 +1,5 @@
 aws cognito-idp sign-up \
---region YOUR_COGNITO_REGION \
+--region us-east-1 \
 --client-id YOUR_COGNITO_APP_CLIENT_ID \
 --username YOUR_USER_EMAIL \
 --password YOUR_USER_PASSWORD
